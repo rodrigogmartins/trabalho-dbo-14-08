eb7d978f038fcd264d07d97ffab1b90c4616a64f
Rodrigo Martins <contawarface750@gmail.com>
2017-09-04 17:50:02 -0300

    Delete functions.js

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-var buildButton = function(nome){
-	var htmlBTN = document.createElement('button');
-	htmlBTN.setAttribute('class', 'btn btn-primary buttons');
-	htmlBTN.setAttribute('href', 'series.html');
-
-	htmlBTN.innerHTML = nome;
-	return document.getElementById('series').appendChild(htmlBTN);
-}
